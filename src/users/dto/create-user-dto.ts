a1a37122b5d8e861c686f2faec1e3983931aacde
nthung1021 <nthung22@clc.fitus.edu.vn>
2025-11-07 10:29:10 +0700

    Create users DTO

---
+++ src/users/dto/create-user-dto.ts
@@ -0,0 +1,15 @@
+import { IsHash, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+    
+    @IsString()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+}
